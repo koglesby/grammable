2deb5f65cee2efaf5c13ae35fca3a970e6db22f2
Kevin Oglesby <kevinjoglesby@gmail.com>
2017-01-10 23:31:25 +0000

    describe grams edit action

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -4,10 +4,14 @@ RSpec.describe GramsController, type: :controller do
   
   describe "grams#edit action" do
     it "should successfully show the edit form if the gram is found" do
+      gram = FactoryGirl.create(:gram)
+      get :edit, id: gram.id
+      expect(response).to have_http_status(:success)
     end
 
     it "should return a 404 message if the gram is not found" do
-
+      get :edit, id: 'POODLES'
+      expect(response).to have_http_status(:not_found)
     end
 
   end
